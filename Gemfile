af207246911f0c24d936bf08995a10670b976105
lavikoder <kevin.kamanda@flatironschool.com>
2019-08-30 19:26:00 +0100

    fix file, error on new instance of Restaurants

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source "https://rubygems.org"
+
+# Specify your gem's dependencies in ten_restaurants_four.gemspec
+gemspec
---
+++ lib/ten_restaurants_four.rb
@@ -0,0 +1,6 @@
+require "ten_restaurants_four/version"
+
+module TenRestaurantsFour
+  class Error < StandardError; end
+  # Your code goes here...
+end
